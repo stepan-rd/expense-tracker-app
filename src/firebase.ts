8deb2a0a325957a0c79971f99d5d94d81e14f6f1
prdel24 <brozikmrozik7@gmail.com>
2024-08-29 06:52:51 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCvJwliVapokxsgAjQpQEUrm_9SeICmDqM",
-  authDomain: "expense-tracker-app-devversion.firebaseapp.com",
-  projectId: "expense-tracker-app-devversion",
-  storageBucket: "expense-tracker-app-devversion.appspot.com",
-  messagingSenderId: "576995008754",
-  appId: "1:576995008754:web:9c7aa6fc94edc31853b06b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app)
-
+export const db = getFirestore(app);
